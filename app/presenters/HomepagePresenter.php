nette-examples/quickstart

implemented blog

app/presenters/HomepagePresenter.php
@@ -1,10 +1,30 @@
 <?php
 
+use Nette\Application\UI\Form;
+
+
+
 class HomepagePresenter extends BasePresenter
 {
 
-	public function renderDefault()
+	/** @var Nette\Database\Connection */
+	private $database;
+
+
+
+	public function __construct(Nette\Database\Connection $database)
 	{
+		$this->database = $database;
+	}
+
+
+
+	public function renderDefault($page = 1)
+	{
+		$this->template->page = $page;
+		$this->template->posts = $this->database->table('posts')
+			->order('created_at DESC')
+			->page($page, 5);
 	}
 
 }

blaskojan/tutorial-quickstart

app/presenters/HomepagePresenter.php
@@ -7,4 +7,22 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	/** @var Nette\Database\Context */
+	private $database;
+
+
+	public function __construct(Nette\Database\Context $database)
+	{
+		$this->database = $database;
+	}
+
+
+	public function renderDefault($page = 1)
+	{
+		$this->template->page = $page;
+		$this->template->posts = $this->database->table('posts')
+			->order('created_at DESC')
+			->page($page, 5);
+	}
+
 }